1f09978fbec515d6670a5e792fe2641f63ec1f32
krikaz <pascal.bou@protonmail.com>
2019-06-21 09:33:02 +0200

    created reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,10 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import smurfsReducer from './reducers';
+import rootReducer from './reducers';
 
 const store = createStore(
-	smurfsReducer, // this is the most basic reducer. A function that returns and object. Replace it.
+	rootReducer, // this is the most basic reducer. A function that returns and object. Replace it.
 	/* be sure to throw in the proper middlewares here*/
 	applyMiddleware(thunk, logger)
 );
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,28 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import * as types from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+ */
+
+const initialState = {
+	smurfs: [],
+	fetchingSmurfs: false,
+	addingSmurf: false,
+	updatingSmurf: false,
+	deletingSmurf: false,
+	error: null,
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +31,42 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export default (rootReducer = (state = initialState, action) => {
+	switch (action.types) {
+		case FETCHING_SMURFS:
+			return {
+				...state,
+				fetchingSmurfs: true,
+			};
+		case FETCHING_SMURFS_SUCCESS:
+			return {
+				...state,
+				fetchingSmurfs: false,
+				smurfs: action.payload,
+			};
+		case FETCHING_SMURFS_FAILURE:
+			return {
+				...state,
+				fetchingSmurfs: false,
+				error: action.payload,
+			};
+		case ADDING_SMURF:
+			return {
+				...state,
+				addingSmurf: true,
+			};
+		case ADDING_SMURF_SUCCESS:
+			return {
+				...state,
+				addingSmurf: false,
+				smurfs: action.payload,
+			};
+		case ADDING_SMURF_FAILURE:
+			return {
+				...state,
+				addingSmurf: false,
+				error: action.payload,
+			};
+	}
+});
