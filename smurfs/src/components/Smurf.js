2b811860a75b0063e74ef9ab64d96516277c6b5d
krikaz <pascal.bou@protonmail.com>
2019-06-21 11:56:36 +0200

    added smurf id

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -4,6 +4,7 @@ export default function Smurf({ smurfs }) {
 	return smurfs.map(smurf => {
 		return (
 			<div key={smurf.id}>
+				<p>{smurf.id}</p>
 				<p>{smurf.name}</p>
 				<p>{smurf.age}</p>
 				<p>{smurf.height}</p>
