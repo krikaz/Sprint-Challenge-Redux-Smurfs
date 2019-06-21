127d0b745f6ea481c739a94320c434e468c14871
krikaz <pascal.bou@protonmail.com>
2019-06-21 11:16:16 +0200

    adding smurf now works

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,34 +1,27 @@
 import React from 'react';
 
+const nameRef = React.createRef();
+const ageRef = React.createRef();
+const heightRef = React.createRef();
+
 export default class SmurfForm extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.nameRef = React.createRef();
-	// 	this.ageRef = React.createRef();
-	//   this.heightRef = React.createRef();
-	// }
-
-	nameRef = React.createRef();
-	ageRef = React.createRef();
-	heightRef = React.createRef();
-
-	newSmurf = {
-		name: this.nameRef.current.value,
-		age: this.ageRef.current.value,
-		height: this.heightRef.current.value,
-	};
-
 	addSmurf() {
-		this.props.addSmurf(this.newSmurf);
+		const newSmurf = {
+			name: nameRef.current.value,
+			age: ageRef.current.value,
+			height: heightRef.current.value,
+		};
+		this.props.props.addSmurf(newSmurf);
 	}
 
 	render() {
+		console.log(this.props);
 		return (
 			<div>
-				<input type="text" ref={this.nameRef} placeholder="name" />
-				<input type="text" ref={this.ageRef} placeholder="age" />
-				<input type="text" ref={this.heightRef} placeholder="height" />
-				<button onClick={this.addSmurf}>Add Smurf</button>
+				<input type="text" ref={nameRef} placeholder="name" />
+				<input type="text" ref={ageRef} placeholder="age" />
+				<input type="text" ref={heightRef} placeholder="height" />
+				<button onClick={this.addSmurf.bind(this)}>Add Smurf</button>
 			</div>
 		);
 	}
