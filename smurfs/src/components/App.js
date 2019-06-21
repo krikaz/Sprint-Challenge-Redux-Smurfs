dcefb04eb874bd7d5f21080cbb96401c8bfbdd14
krikaz <pascal.bou@protonmail.com>
2019-06-21 09:57:37 +0200

    import actions in App.js and connect

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,17 +6,34 @@ import './App.css';
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+import * as actions from './actions';
+import connect from 'react-redux';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>SMURFS! 2.0 W/ Redux</h1>
+				<div>Welcome to your Redux version of Smurfs!</div>
+				<div>Start inside of your `src/index.js` file!</div>
+				<div>Have fun!</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return {
+		smurfs: state.rootReducer.smurfs,
+		fetchingSmurfs: state.rootReducer.fetchingSmurfs,
+		addingSmurf: state.rootReducer.addingSmurf,
+		updatingSmurf: state.rootReducer.updatingSmurf,
+		deletingSmurf: state.rootReducer.deletingSmurf,
+		error: state.rootReducer.error,
+	};
+}
+
+export default connect(
+	mapStateToProps,
+	actions
+)(App);
